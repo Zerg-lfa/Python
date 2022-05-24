ffea5fbe61d6a1ced25df01202ee56affc1827f6
Vladislav <v.fanin2010@yandex.ru>
2022-05-24 19:21:29 +0300

    create hw 4

---
+++ HW4_2/task_4.py
@@ -0,0 +1,6 @@
+import utils
+
+utils.currency_rates('INR')
+utils.currency_rates('HKD')
+utils.currency_rates('EUR')
+utils.currency_rates('NOK')
---
+++ HW4_2/utils.py
@@ -0,0 +1,17 @@
+import requests
+import decimal
+
+
+def currency_rates(currents='USD', url='http://www.cbr.ru/scripts/XML_daily.asp'):
+    try:
+        site = requests.get(url)
+        site_date = site.headers.get('date')
+        site_content = site.text
+        cur = site_content.split(currents.upper())
+        ind_cur = (cur[1].find('Value'))
+        course = cur[1][ind_cur + 6:ind_cur + 13]
+        course = course.replace(',', '.')
+        course = decimal.Decimal(course)
+        print(f'{course} руб., {site_date}')
+    except:
+        print(None)
