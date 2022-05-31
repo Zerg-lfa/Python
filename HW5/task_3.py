ef86055d8d677b3f2660c97ef7516e91fce81948
Vladislav <v.fanin2010@yandex.ru>
2022-05-31 21:06:52 +0300

    correct

--- HW5/task_3.py
+++ HW5/task_3.py
@@ -1,8 +1,8 @@
-import itertools
+from itertools import zip_longest
 
 tutors = ['Иван', 'Анастасия', 'Петр', 'Сергей', 'Дмитрий', 'Борис', 'Елена', 'Дмитрий', 'Борис', 'Елена']
 klasses = ['9А', '7В', '9Б', '9В', '8Б', '10А']
 
-result_list = (itertools.zip_longest(tutors, klasses))
+result_list = (zip_longest(tutors, klasses))
 for i in result_list:
     print(i)
